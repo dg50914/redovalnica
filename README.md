c55dd775049e4e7670fe198daa4210377bbd21e8
dg50914 <dg50914@student.uni-lj.si>
2025-12-02 19:13:57 +0100

    Add documentation

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # redovalnica
+
+Paket redovalnica ponuja preprost način upravljanja z ocenami študentov, prikazovanjem redovalnice in izračunavanjem povprečij.
+
+Demo program sprejema tri argumente:
+
+- `--stOcen <n>` določa minimalno število ocen, ki jih študent potrebuje, da se mu izračuna povprečje
+- `--minOcena <n>` določa minimalno oceno, ki jo bo redovalnica sprejela
+- `--maxOcena <n>` določa maksimalno oceno, ki jo bo redovalnica sprejela
--- redovalnica/redovalnica.go
+++ redovalnica/redovalnica.go
@@ -1,7 +1,42 @@
+// Paket redovalnica ponuja preprost način upravljanja z ocenami študentov, prikazovanjem redovalnice in izračunavanjem povprečij.
+//
+// Primer uporabe:
+//
+//	studenti := map[string]redovalnica.Student{
+//		"123": {
+//			Ime:     "Franc",
+//			Priimek: "Horvat",
+//			Ocene:   []int{10, 9, 10, 9, 10, 10, 9},
+//		},
+//		"456": {
+//			Ime:     "Ana",
+//			Priimek: "Kovačič",
+//			Ocene:   []int{6, 7, 6, 7, 6, 7, 6, 7},
+//		},
+//		"789": {
+//			Ime:     "Janez",
+//			Priimek: "Novak",
+//			Ocene:   []int{4, 5, 6, 5, 4, 5, 4, 5, 4},
+//		},
+//		"234": {
+//			Ime:     "Zdravo",
+//			Priimek: "Neocenjen",
+//			Ocene:   []int{7, 8, 9},
+//		},
+//	}
+//
+//	redovalnica.IzpisVsehOcen(studenti)
+//
+//	redovalnica.DodajOceno(studenti, "123", 8, minOcena, maxOcena)
+//	redovalnica.IzpisVsehOcen(studenti)
+//
+//	redovalnica.IzpisiKoncniUspeh(studenti, stOcen)
+
 package redovalnica
 
 import "fmt"
 
+// Student predstavlja enega študenta
 type Student struct {
 	Ime     string
 	Priimek string
@@ -12,6 +47,7 @@ func (s Student) String() string {
 	return fmt.Sprintf("%s %s", s.Ime, s.Priimek)
 }
 
+// DodajOceno danemu študentu doda oceno, če se ta nahaja med min in max
 func DodajOceno(studenti map[string]Student, vpisnaStevilka string, ocena int, min, max int) {
 	if !(ocena >= min && ocena <= max) {
 		fmt.Printf("Ocena %d ni v intervalu [%d, %d]\n", ocena, min, max)
@@ -30,6 +66,7 @@ func DodajOceno(studenti map[string]Student, vpisnaStevilka string, ocena int, m
 	fmt.Printf("Ocena %d dodana študentu/ki %s\n", ocena, student)
 }
 
+// IzpisVsehOcen izpiše vse študente z vpisnimi številkami in njihove ocene
 func IzpisVsehOcen(studenti map[string]Student) {
 	fmt.Println("REDOVALNICA:")
 	for vpisna, student := range studenti {
@@ -38,6 +75,9 @@ func IzpisVsehOcen(studenti map[string]Student) {
 	}
 }
 
+// povprecje izračuna povprečno oceno študenta.
+// Če ima študent manj kot minStOcen ocen, vrne 0.
+// Če študenta ne more najti, vrne -1.
 func povprecje(studenti map[string]Student, vpisnaStevilka string, minStOcen int) float64 {
 	student, ok := studenti[vpisnaStevilka]
 	if !ok {
@@ -57,6 +97,10 @@ func povprecje(studenti map[string]Student, vpisnaStevilka string, minStOcen int
 	return avg
 }
 
+// IzpisiKoncniUspeh izpiše vse študente, njihove povprečene ocene in dosežen uspeh
+// Če je povprečje < 6, je uspeh neuspešen
+// Če je povprečje >= 6 in povprečje < 9, je uspeh povprečen
+// Če je povprečje >=, je uspeh odličen
 func IzpisiKoncniUspeh(studenti map[string]Student, minStOcen int) {
 	fmt.Println("KONČNI USPEH:")
 	for vpisna, student := range studenti {
